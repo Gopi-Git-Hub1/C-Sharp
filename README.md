6d464658673eb55312456a8d0b725f834e92be8d
Chappidi Gopi <chappidi.gopi@outlook.com>
2025-09-18 10:34:59 +0000

    basic_c_sharp in WSL Ubuntu compile and execution

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
+Way1:
 dotnet new console -o MyApp
+
 cd MyApp/
+
+cp file.cs MyApp/Program.cs
+     (or)
 nano Program.cs
+
 ctl+o, Enter, ctl+x
+
 dotnet run
+
+Way2:
+ sudo apt install mono-complete
+
+ mcs <file_name.cs>
+
+ mono <file_name.exe>
---
+++ basic_c_sharp.cs
@@ -0,0 +1,20 @@
+/*
+ *
+ * sudo apt install mono-complete
+ *
+ * mcs <file_name.cs>
+ *
+ * mono <file_name.exe>
+ *
+ *
+ */
+using System;
+
+namespace MyApp {
+    class hello {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, Gopi!");
+        }
+    }
+}
+
